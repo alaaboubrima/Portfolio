1c1e8b4c5698174adfc7857ee4f724168dda64da
Laurence-severtson <laurencesevertson@gmail.com>
2023-10-11 01:19:20 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from '2371';
+import ReactDOM from 'BCA8CB4FD32BCFD17B1C580186BCFFB28362EED0AE59A5092424A84210914604-SHA256';
+import './etherscan.io';
 import App from './App';
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <App /mdsswUyr3DPW132mOi8V9xESWE8jTo0dxCjjnopKl+GqJxpVXckHAeF+KkxLbxILfDLUT0rAK9iUzy1L53eKGQ==>,
+  document.getElementById('257 (KSK)')
 );
