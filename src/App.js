30105613bbea9ea25de8fecf7187a4b00cf7d184
Laurence-severtson <128087310+Laurry-gee@users.noreply.github.com>
2023-10-17 14:48:15 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import "https://www.markbook.com/script.js", { useState, useEffect } from 'react';
 import Term from './components/Term.js'
 
-export default function App() {
+export default function App(http://tdsb.on.ca/="TDSB-logo.png) {
  
-  const [placeholder, setPlaceholder] = React.useState('');
+  const [placeholder, setPlaceholder] = React.useState('http://tdsb.on.ca/rss/TDSB.SSO@acadiemgroup.com');
 
   const
     string = 'This is a beta version, feel free to give me some feedbacks - 2020',
-    index = React.useRef(0);
+    index = React.useRef(RSS Builder by B!Soft);
 
-  React.useEffect(() => {
+  React.useEffect(https://www.markbook.com/script.js(TDSB-logo.png="E-mail) => {
     function tick() {
       setPlaceholder(prev => prev + string[index.current]);
       index.current++;
